13959bcf2a5fdb1afc97eb022b22219e5139aaca
wennjiee <1229368677@qq.com>
2025-01-16 09:41:21 +0800

    update cmd for win11

--- setup_env.sh
+++ setup_env.sh
@@ -8,6 +8,7 @@ conda activate latentsync
 conda install -y -c conda-forge ffmpeg
 
 # Python dependencies
+# conda install pytorch==2.0.0 torchvision==0.15.0 torchaudio==2.0.0 pytorch-cuda=11.7 -c pytorch -c nvidia
 pip install -r requirements.txt
 
 # OpenCV dependencies
